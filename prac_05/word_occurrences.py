af6b4fa2aaf3e975888fe6364d5bf42ef2a1949a
ChuqiaoW <81671915+ChuqiaoW@users.noreply.github.com>
2021-04-20 12:15:57 +0800

    Update word_occurrences.py

--- prac_05/word_occurrences.py
+++ prac_05/word_occurrences.py
@@ -4,7 +4,7 @@ for each in text:
     if each not in dictionary:
         dictionary[each] = 1
     else:
-        dictionary[each] += 1
+        dictionary[each] = dictionary[each]+1
 length = max(len(x) for x in dictionary)
 for key, value in sorted(dictionary.items()):
     print(f"{key:<{length}} : {value}")
