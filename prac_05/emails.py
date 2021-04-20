2f146aeae1c841849533e15d31655185fa69811e
ChuqiaoW <81671915+ChuqiaoW@users.noreply.github.com>
2021-04-20 16:10:55 +0800

    Update emails.py

--- prac_05/emails.py
+++ prac_05/emails.py
@@ -5,9 +5,9 @@ while email != [""]:
     confirm = str(input(f"Is your name {name_unconfirmed}? (Y/n)")).lower()
     if confirm == "no" or confirm == "n":
         confirmed_name = input("Name:")
-        NAME_DICT[confirmed_name] = ''.join(email)
+        NAME_DICT[confirmed_name] = '@'.join(email)
     elif confirm == "" or confirm == "y":
-        NAME_DICT[name_unconfirmed] = ''.join(email)
+        NAME_DICT[name_unconfirmed] = '@'.join(email)
     email = input("Email:").split("@")
 for key, value in NAME_DICT.items():
     print(f"{key} ({value})")
