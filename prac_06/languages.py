d13c40ccefc5a191d369da4104137bb3f30d4e09
Wu Chuqiao <chuqiao.wu@my.jcu.edu.au>
2021-04-26 19:25:49 +0800

    prac_06

--- prac_06/languages.py
+++ prac_06/languages.py
@@ -1,5 +1,4 @@
 from prac_06.programming_language import ProgrammingLanguage
-from operator import attrgetter
 ruby = ProgrammingLanguage("Ruby", "Dynamic", True, 1995)
 python = ProgrammingLanguage("Python", "Dynamic", True, 1991)
 visual_basic = ProgrammingLanguage("Visual Basic", "Static", False, 1991)
